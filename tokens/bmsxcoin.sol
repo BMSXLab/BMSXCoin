a5ab3bdd881aa0f961bce04a56706a8207cfab0f
Rainer Feike <rainer@feike.de>
2022-10-22 10:27:18 +0200

    Create bmsxcoin contract and token

---
+++ tokens/bmsxcoin.sol
@@ -0,0 +1,121 @@
+pragma solidity ^0.4.24;
+ 
+// Code to create the BMSX ERC-20 token
+// Code based on https://www.quicknode.com/guides/smart-contract-development/how-to-create-and-deploy-an-erc20-token
+// ICO Wallet on Goerli Testnet is 0xE7D7e51332218EC346dAf4f33Ff7F639d4186a26
+// Initial fill for gas is https://goerli.etherscan.io/tx/0x9ffbc68d940c42200f190480d0ad1958ec736cbdd5cfc89d2d8dc3fbeec616ed
+//
+// Rainer Feike, Okt. 2022
+
+// Safe Math Interface
+ 
+contract SafeMath {
+ 
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+ 
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+ 
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+ 
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+ 
+ 
+// ERC Token Standard #20 Interface
+ 
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+ 
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+ 
+ 
+// Contract function to receive approval and execute function in one call
+ 
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+}
+ 
+// Actual token contract
+ 
+contract BMSXToken is ERC20Interface, SafeMath {
+    string public symbol;
+    string public  name;
+    uint8 public decimals;
+    uint public _totalSupply;
+ 
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+ 
+    constructor() public {
+        symbol = "BMSX";
+        name = "BMSX Coin";
+        // create 1 Mio. tokens
+        decimals = 2;
+        _totalSupply = 100000000;
+        balances[0xE7D7e51332218EC346dAf4f33Ff7F639d4186a26] = _totalSupply;
+        emit Transfer(address(0), 0xE7D7e51332218EC346dAf4f33Ff7F639d4186a26, _totalSupply);
+    }
+ 
+    function totalSupply() public constant returns (uint) {
+        return _totalSupply  - balances[address(0)];
+    }
+ 
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+        return balances[tokenOwner];
+    }
+ 
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+ 
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+ 
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+ 
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+ 
+    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true;
+    }
+ 
+    function () public payable {
+        revert();
+    }
+}
